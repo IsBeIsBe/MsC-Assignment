d0c6897895463ec2bf246c7fabfa91fa71ad250c
maxine-jay <maxinejaysimpson@gmail.com>
2019-02-04 21:05:26 +0000

    database functioning

--- src/commandline/DatabaseInteraction.java
+++ src/commandline/DatabaseInteraction.java
@@ -3,231 +3,245 @@ package commandline;
 import java.sql.*;
 
 /**
- * The DatabaseInteraction class handles submitting and recalling information from the Database. It is sent the relevant data 
- * from the Game class in the first instance, and stores the relevant information as private variables in the latter.  
+ * The DatabaseInteraction class handles submitting and recalling information
+ * from the Database. It is sent the relevant data from the Game class in the
+ * first instance, and stores the relevant information as private variables in
+ * the latter.
  */
 public class DatabaseInteraction {
-    private int gamesPlayed;
-    private int playerWins;
-    private int compWins;
-    private int avgDraws;
-    private int longestRound;
-    private int game_number;
+	private int gamesPlayed;
+	private int humanWins;
+	private int compWins;
+	private int avgDraws;
+	private int longestRound;
 
-    //database sign in credentials
-    static final String USERNAME = "m_18_2208247s";
-    static final String PASSWORD = "2208247s";
-                
-    //jdbc driver name and database url
-    static final String JDBC_DRIVER = "org.postgresql.Driver";
-    static final String DB_URL = "jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/m_18_2208247s";
-    //tried updating with username in URL as indicated in lecture slides...didn't work
+	// database sign in credentials
+	static final String USERNAME = "m_18_2208247s";
+	static final String PASSWORD = "2208247s";
 
+	// jdbc driver name and database url
+	static final String JDBC_DRIVER = "org.postgresql.Driver";
+	static final String DB_URL = "jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/m_18_2208247s";
+	// tried updating with username in URL as indicated in lecture slides...didn't
+	// work
 
-        public DatabaseInteraction(){
+	public DatabaseInteraction() {
 
-        }
+	}
 
-        /**
-         * Using the results of each game, a static method is used to update the database with the relevant data. 
-         * It uses the individuals scores to determine the overall winner of the game. 
-         *  
-         * @param gameResults
-         */
-        public static void insertGameStats(int[] gameResults){
-             //load JDBC Driver
+	/**
+	 * Using the results of each game, a static method is used to update the
+	 * database with the relevant data. It uses the individuals scores to determine
+	 * the overall winner of the game.
+	 * 
+	 * @param gameResults
+	 */
+	public static void insertGameStats(int[] gameResults) {
+		// load JDBC Driver
 
-             int humanRoundWins = gameResults[0]; 
-             int aiOneWins = gameResults[1];
-             int aiTwoWins = gameResults[2];
-             int aiThreeWins = gameResults[3];
-             int aiFourWins = gameResults[4];
-             int numOfRounds = gameResults[5];
-             int drawsNumber = gameResults[6];
-             String overallGameWinner = calculateOverallWinner(gameResults);
+		int humanRoundWins = gameResults[0];
+		int aiOneWins = gameResults[1];
+		int aiTwoWins = gameResults[2];
+		int aiThreeWins = gameResults[3];
+		int aiFourWins = gameResults[4];
+		int numOfRounds = gameResults[5];
+		int drawsNumber = gameResults[6];
+		String overallGameWinner = calculateOverallWinner(gameResults);
 
-             try{
-                Class.forName(JDBC_DRIVER);
-                } catch (ClassNotFoundException e){
-                    System.out.println("Could not find JDBC Driver");
-                    e.printStackTrace();
-                    return;
-                    }
-                    
-                    System.out.println("Driver found");
-                    
-                    Connection connection = null;
-                    Statement stmt = null;
-                    
-                    
-                    
-                    try {
-                    System.out.println("Connecting to database...");
-                    connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-                    System.out.println("Succesfully connected!");
-                    } catch(SQLException e){
-                    System.out.println("Connection Failed");
-                    e.printStackTrace();
-                    return;
-                    }
-                    
-                    if (connection != null){
-                    
-                    try{
-                    System.out.println("Inserting info into table...");
-                    stmt = connection.createStatement();
-                    //First query
-                    String insert = "INSERT INTO game(humanRoundWins, aiOneWins, aiTwoWins, aiThreeWins, aiFourWins, numOfRounds, drawsNumber, overallGameWinner) VALUES(" + humanRoundWins + ","  +  aiOneWins+ "," + aiTwoWins+ "," + aiThreeWins+ "," + aiFourWins+ "," + numOfRounds+ "," + drawsNumber+ "," + overallGameWinner + ")";
-                    stmt.executeUpdate(insert);
-                    
-                
-                    
-                    }catch (SQLException e){
-                    e.printStackTrace();
-                    }
-                }
+		try {
+			Class.forName(JDBC_DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Could not find JDBC Driver");
+			e.printStackTrace();
+			return;
+		}
 
+		System.out.println("Driver found");
 
-        }
-                
-        /**
-         * The getGameStats method returns the required information outlined in the requirements. 
-         * 
-         * Number of games is calculated by returning a count of the tuples in the table. 
-         * Number of AI wins is calculated by searching the 'overall winner column for entries not including 
-         * the word 'human'. This number is subtracted from the number of games to find the number of human wins. 
-         * The average number of draws and largest number of rounds are calculated using SQL queries. 
-         */
-        public void getGameStats(){
-      
-//        	int humanRoundWins = gameResults[0]; 
-//            int aiOneWins = gameResults[1];
-//            int aiTwoWins = gameResults[2];
-//            int aiThreeWins = gameResults[3];
-//            int aiFourWins = gameResults[4];
-//            int numOfRounds = gameResults[5];
-//            int drawsNumber = gameResults[6];
-//            String overallGameWinner = calculateOverallWinner(gameResults);
-            
-            try{
-           
-            Class.forName(JDBC_DRIVER);
-            } catch (ClassNotFoundException e){
-                System.out.println("Game stats - Could not find JDBC Driver");
-                e.printStackTrace();
-                return;
-                }
-                
-                System.out.println("Game stats - Driver found");
-                
-                Connection connection = null;
-                Statement stmt = null;
-                
-                
-                
-                try {
-                System.out.println("Game stats - Connecting to database...");
-                connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-                System.out.println("Game stats - Succesfully connected\n");
-                } catch(SQLException e){
-                System.out.println("Game stats - Connection Failed");
-                e.printStackTrace();
-                return;
-                }
-                
-               
-                if (connection != null){
-                
-                try{
-                	
-                Statement statement = connection.createStatement();
-                String q1 = "SELECT COUNT (humanroundwins) AS count FROM game";                
-                ResultSet  rs = statement.executeQuery(q1);
-              
-                while (rs.next()) {
-                this.game_number = rs.getInt("count");
-                      
-				//First query
-//                rs = statement.executeQuery("SELECT COUNT (humanroundwins) AS 'count'" +
-//                " FROM game");
-//                this.game_number = rs.getInt("count");
+		Connection connection = null;
+		Statement stmt = null;
 
-//                //second query
-//                rs = statement.executeQuery("SELECT COUNT (overallGameWinner) AS AIWins"
-//               + " FROM game" +
-//                " WHERE 'overallGameWinner' <> 'humanRoundWins'");
-//                this.compWins = rs.getInt("AIWins");
-//                this.playerWins = gamesPlayed - compWins;
-//
-//                //Getting averages   
-//                rs = statement.executeQuery("SELECT AVG (drawsNumber) AS numDraws" + 
-//                " FROM game");
-//                this.avgDraws = rs.getInt("numDraws");
-//
-//                //Longest Round
-//                rs = statement.executeQuery("SELECT MAX (numOfRounds) AS biggestRound" +
-//                " FROM game");
-//                this.longestRound = rs.getInt("biggestRound");
-                	
-               
-                }
-                rs.close();
-                }catch (SQLException e ){
-                e.printStackTrace();
-                }
-            }
-                
-        }
+		try {
+			System.out.println("Connecting to database...");
+			connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+			System.out.println("Succesfully connected!");
+		} catch (SQLException e) {
+			System.out.println("Connection Failed");
+			e.printStackTrace();
+			return;
+		}
 
-        public int getGamesPlayed(){
-            return this.gamesPlayed;
-        }
+		if (connection != null) {
 
-        public int getPlayerWins(){
-            return this.playerWins;
-        }
+			try {
+				System.out.println("Inserting info into table...");
+				stmt = connection.createStatement();
+				// First query
+				String insert = "INSERT INTO game(humanRoundWins, aiOneWins, aiTwoWins, aiThreeWins, aiFourWins, numOfRounds, drawsNumber, overallGameWinner) VALUES("
+						+ humanRoundWins + "," + aiOneWins + "," + aiTwoWins + "," + aiThreeWins + "," + aiFourWins
+						+ "," + numOfRounds + "," + drawsNumber + "," + overallGameWinner + ")";
+				stmt.executeUpdate(insert);
 
-        public int getCompWins(){
-            return this.compWins;
-        }
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 
-        public int getAvgDraws(){
-            return this.avgDraws;
-        }
+	}
 
-        public int getLongestRound(){
-            return this.longestRound;
-        }
+	/**
+	 * The getGameStats method returns the required information outlined in the
+	 * requirements.
+	 * 
+	 * Number of games is calculated by returning a count of the tuples in the
+	 * table. Number of AI wins is calculated by searching the 'overall winner
+	 * column for entries not including the word 'human'. This number is subtracted
+	 * from the number of games to find the number of human wins. The average number
+	 * of draws and largest number of rounds are calculated using SQL queries.
+	 */
+	public void getGameStats() {
 
-        /**
-         * This method calculates the overall winner of the game through finding the highest individual round score. 
-         * @param gameResults
-         * @return winnerResult
-         */
-        public static String calculateOverallWinner(int[] gameResults){
-            int winner = 0;
-            String winnerResult = "";
-            for(int i = 0; i <4; i++){
-                if (gameResults[i] > winner){
-                    winner = i;
-                }
-            }
+		try {
 
-            if (winner == 0){
-                winnerResult = "'Human'";
-            }
-            if (winner == 1){
-                winnerResult = "'AI player One'";
-            }
-            if (winner == 2){
-                winnerResult = "'AI player Two'";
-            }
-            if (winner == 3){
-                winnerResult = "'AI player Three'";
-            }
-            if (winner == 4){
-                winnerResult = "'AI player Four'";
-            }
-            return winnerResult;
-        }
-    }
+			Class.forName(JDBC_DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Game stats - Could not find JDBC Driver");
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println("Game stats - Driver found");
+
+		Connection connection = null;
+		Statement stmt = null;
+
+		try {
+			System.out.println("Game stats - Connecting to database...");
+			connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+			System.out.println("Game stats - Succesfully connected\n");
+		} catch (SQLException e) {
+			System.out.println("Game stats - Connection Failed");
+			e.printStackTrace();
+			return;
+		}
+
+		if (connection != null) {
+
+			try {
+
+				Statement statement = connection.createStatement();
+
+				// First Query
+				String queryGamesPlayed = "SELECT COUNT(*)humanroundwins FROM game";
+				ResultSet rsGamesPlayed = statement.executeQuery(queryGamesPlayed);
+
+				while (rsGamesPlayed.next()) {
+					this.gamesPlayed = rsGamesPlayed.getInt(1);
+				}
+				// 1 is the column index here
+
+				// Second Query
+				String queryAIWinner = "SELECT COUNT(*) overallGameWinner FROM game WHERE 'overallGameWinner' <> 'Human'";
+				ResultSet rsAIWinner = statement.executeQuery(queryGamesPlayed);
+
+				while (rsAIWinner.next()) {
+					this.compWins = rsAIWinner.getInt(1);
+//					this.humanWins = gamesPlayed - compWins;
+				}
+
+				// Third Query
+//				********************************************CHECK ABOVE WITH DATABASE
+//				***************************************************************************
+//				***************************************************************************
+				String queryHumanWinner = "SELECT COUNT(*) overallGameWinner FROM game WHERE 'overallGameWinner' = 'Human'";
+				ResultSet rsHumanWinner = statement.executeQuery(queryHumanWinner);
+
+				while (rsHumanWinner.next()) {
+					this.humanWins = rsHumanWinner.getInt(1);
+				}
+
+				// Fourth Query
+				String queryAvgDraws = "SELECT AVG(drawsNumber) FROM game";
+
+				ResultSet rsAvgDraws = statement.executeQuery(queryAvgDraws);
+				while (rsAvgDraws.next()) {
+					this.avgDraws = rsAvgDraws.getInt(1);
+
+				}
+
+				// Fifth Query
+				String queryMaxRounds = "SELECT MAX(numOfRounds) FROM game";
+
+				ResultSet rsMaxRounds = statement.executeQuery(queryMaxRounds);
+				while (rsMaxRounds.next()) {
+					this.longestRound = rsMaxRounds.getInt(1);
+				}
+
+				// close all result sets
+				rsGamesPlayed.close();
+				rsAIWinner.close();
+				rsHumanWinner.close();
+				rsAvgDraws.close();
+				rsMaxRounds.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public int getGamesPlayed() {
+		return this.gamesPlayed;
+	}
+
+	public int getHumanWins() {
+		return this.humanWins;
+	}
+
+	public int getCompWins() {
+		return this.compWins;
+	}
+
+	public int getAvgDraws() {
+		return this.avgDraws;
+	}
+
+	public int getLongestRound() {
+		return this.longestRound;
+	}
+
+	/**
+	 * This method calculates the overall winner of the game through finding the
+	 * highest individual round score.
+	 * 
+	 * @param gameResults
+	 * @return winnerResult
+	 */
+	public static String calculateOverallWinner(int[] gameResults) {
+		int winner = 0;
+		String winnerResult = "";
+		for (int i = 0; i < 4; i++) {
+			if (gameResults[i] > winner) {
+				winner = i;
+			}
+		}
+
+		if (winner == 0) {
+			winnerResult = "'Human'";
+		}
+		if (winner == 1) {
+			winnerResult = "'AI player One'";
+		}
+		if (winner == 2) {
+			winnerResult = "'AI player Two'";
+		}
+		if (winner == 3) {
+			winnerResult = "'AI player Three'";
+		}
+		if (winner == 4) {
+			winnerResult = "'AI player Four'";
+		}
+		return winnerResult;
+	}
+}
--- src/commandline/TopTrumpsCLIApplication.java
+++ src/commandline/TopTrumpsCLIApplication.java
@@ -44,7 +44,6 @@ public class TopTrumpsCLIApplication {
 					if (selection == 1) {
 						System.out.print("Printing Game Stats... \n");
 						printGameStatistics();
-						
 						break;
 				
 					} else if (selection == 2) {
@@ -97,11 +96,11 @@ public class TopTrumpsCLIApplication {
 		    db.getGameStats(); 
 
 		    System.out.println("Game Statistics: \n" +
-		    "Number of Games: " + db.getGamesPlayed() + "\n"); 
-//		    "Number of Human Wins: " + db.getPlayerWins() + "\n" +
-//		    "Number of AI Wins: " + db.getCompWins() + "\n" +
-//		    "Average Number of Draws: " + db.getAvgDraws() + "\n" +
-//		    "Longest Game: " + db.getLongestRound());
+		    "Number of Games: " + db.getGamesPlayed() + "\n" +
+		    "Number of Human Wins: " + db.getHumanWins() + "\n" +
+		    "Number of AI Wins: " + db.getCompWins() + "\n" +
+		    "Average Number of Draws: " + db.getAvgDraws() + "\n" +
+		    "Longest Game: " + db.getLongestRound() + " rounds");
 		}
 
 }
